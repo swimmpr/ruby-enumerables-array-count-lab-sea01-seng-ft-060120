12ec481f95954bde6cc4f529baaf0ea7ba248fc4
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-17 21:18:55 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,8 +3,8 @@ require 'pry'
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   
-# array.count do |element| 
-# element.instance_of? (String)
+array.count do |element| 
+element == String
  
 end
 
